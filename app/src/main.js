bf4a6acd827b60b70b0a4f3dba1761510ed0011f
Ludovic THUARD <thuard.ludovic@gmail.com>
2023-02-19 10:22:03 +0100

    Add PrimeVue

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Dialog from 'primevue/dialog';
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app
+.use(createPinia())
+.use(PrimeVue);
+
+app.component('Dialog', Dialog);
 
 app.mount("#app");
